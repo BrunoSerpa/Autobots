857d1bfd466b7034c810549629d9d8d73e76076f
BrunoSerpa <serpabruno2005@gmail.com>
2025-08-10 13:16:03 -0300

    Inseri as rotas de Telefone

---
+++ src/main/java/com/autobots/automanager/controles/TelefoneControle.java
@@ -0,0 +1,54 @@
+package com.autobots.automanager.controles;
+
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.autobots.automanager.dto.TelefoneDTO;
+import com.autobots.automanager.servicos.TelefoneServico;
+
+@RestController
+@RequestMapping("/telefone")
+public class TelefoneControle {
+	private TelefoneServico telefoneServico;
+
+	public TelefoneControle(TelefoneServico telefoneServico) {
+		this.telefoneServico = telefoneServico;
+	}
+
+	@GetMapping("/{id}")
+	public ResponseEntity<TelefoneDTO> procurarTelefone(@PathVariable Long id) {
+		return ResponseEntity.status(HttpStatus.OK).body(telefoneServico.procurar(id));
+	}
+
+	@GetMapping
+	public ResponseEntity<List<TelefoneDTO>> todosTelefones() {
+		return ResponseEntity.status(HttpStatus.OK).body(telefoneServico.todos());
+	}
+
+	@PostMapping("/{idCliente}")
+	public ResponseEntity<Void> cadastroTelefone(@PathVariable Long idCliente, @RequestBody TelefoneDTO telefone) {
+		telefoneServico.cadastro(idCliente, telefone);
+		return ResponseEntity.status(HttpStatus.CREATED).build();
+	}
+
+	@PutMapping("/{id}")
+	public ResponseEntity<TelefoneDTO> atualizarTelefone(@PathVariable Long id, @RequestBody TelefoneDTO telefone) {
+		return ResponseEntity.status(HttpStatus.CREATED).body(telefoneServico.atualizar(id, telefone));
+	}
+
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> excluirTelefone(@PathVariable Long idCliente, @PathVariable Long id) {
+		telefoneServico.excluir(id);
+		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+	}
+}
---
+++ src/main/java/com/autobots/automanager/servicos/TelefoneServico.java
@@ -0,0 +1,92 @@
+package com.autobots.automanager.servicos;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.autobots.automanager.converter.TelefoneConverter;
+import com.autobots.automanager.dto.ClienteDTO;
+import com.autobots.automanager.dto.TelefoneDTO;
+import com.autobots.automanager.entidades.Cliente;
+import com.autobots.automanager.entidades.Telefone;
+import com.autobots.automanager.modelo.TelefoneAtualizador;
+import com.autobots.automanager.repositorios.ClienteRepositorio;
+import com.autobots.automanager.repositorios.TelefoneRepositorio;
+import com.autobots.automanager.validar.TelefoneValidar;
+
+@Service
+public class TelefoneServico {
+	private static final String NAO_ENCONTRADO = "Telefone não encontrado";
+
+	private ClienteServico servicoCliente;
+	private ClienteRepositorio repositorioCliente;
+	private TelefoneAtualizador atualizador;
+	private TelefoneConverter conversor;
+	private TelefoneRepositorio repositorio;
+	private TelefoneValidar validar;
+
+	public TelefoneServico(ClienteServico servicoCliente,
+			ClienteRepositorio repositorioCliente,
+			TelefoneAtualizador atualizador,
+			TelefoneConverter conversor,
+			TelefoneRepositorio repositorio,
+			TelefoneValidar validar) {
+		this.servicoCliente = servicoCliente;
+		this.atualizador = atualizador;
+		this.conversor = conversor;
+		this.repositorio = repositorio;
+		this.repositorioCliente = repositorioCliente;
+		this.validar = validar;
+	}
+
+	public TelefoneDTO procurar(Long id) {
+		Telefone telefone = repositorio.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException(NAO_ENCONTRADO));
+		return conversor.convertToDto(telefone);
+	}
+
+	public List<TelefoneDTO> todos() {
+		List<Telefone> telefones = repositorio.findAll();
+		return conversor.convertToDto(telefones);
+	}
+
+	public TelefoneDTO cadastro(Long idCliente, TelefoneDTO telefoneDTO) {
+		List<String> erros = validar.verificar(telefoneDTO);
+		if (!erros.isEmpty()) {
+			StringBuilder mensagem = new StringBuilder();
+			mensagem.append("Falta os dados:");
+			erros.forEach(erro -> mensagem.append("\n").append(erro));
+			throw new IllegalArgumentException(mensagem.toString());
+		}
+
+		ClienteDTO cliente = servicoCliente.procurar(idCliente);
+
+		cliente.getTelefones().add(telefoneDTO);
+		servicoCliente.atualizar(idCliente, cliente);
+
+		return cliente.getTelefones().get(cliente.getTelefones().size() - 1);
+	}
+
+	public TelefoneDTO atualizar(Long id, TelefoneDTO telefoneDTO) {
+		Telefone telefone = repositorio.findById(id)
+				.orElseThrow(() -> new IllegalArgumentException(NAO_ENCONTRADO));
+
+		atualizador.atualizar(telefone, conversor.convertToEntity(telefoneDTO));
+		repositorio.save(telefone);
+
+		return conversor.convertToDto(telefone);
+	}
+
+	public void excluir(Long id) {
+		Cliente cliente = repositorioCliente.findOneByTelefonesId(id)
+				.orElseThrow(() -> new IllegalArgumentException(NAO_ENCONTRADO));
+
+		Telefone telefone = cliente.getTelefones().stream()
+				.filter(t -> t.getId().equals(id))
+				.findFirst()
+				.orElseThrow(() -> new IllegalArgumentException(NAO_ENCONTRADO));
+		cliente.getTelefones().remove(telefone);
+
+		repositorioCliente.save(cliente);
+	}
+}
