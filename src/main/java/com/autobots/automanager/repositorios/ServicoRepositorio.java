0756ed405410f0e5174fccbf76b9201480d88af1
BrunoSerpa <serpabruno2005@gmail.com>
2025-10-02 09:28:22 -0300

    Correção do repositório

--- src/main/java/com/autobots/automanager/repositorios/ServicoRepositorio.java
+++ src/main/java/com/autobots/automanager/repositorios/ServicoRepositorio.java
@@ -2,7 +2,7 @@ package com.autobots.automanager.repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.autobots.automanager.entidades.Veiculo;
+import com.autobots.automanager.entidades.Servico;
 
-public interface ServicoRepositorio extends JpaRepository<Veiculo, Long> {
+public interface ServicoRepositorio extends JpaRepository<Servico, Long> {
 }
